645731945f7ca7b47f19869d6055349822a1c135
Sandesh Shrestha <sandesh@tekkon.io>
2020-08-27 23:45:09 +0545

    asdfowqjefsdf

--- server.js
+++ server.js
@@ -28,17 +28,13 @@ mongoose
 // Use Routes
 app.use('/api/items', items)
 
-
-if (process.env.NODE_ENV === 'production') {
-    // Set a static folder
-    app.use(express.static(path.join(__dirname, "client/build")))
-
-    // Send all requests in build index
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-    });
-
-    console.log("Sent file to build index.html")
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ))
+    })
 }
 
 const port = process.env.PORT || 5000;
