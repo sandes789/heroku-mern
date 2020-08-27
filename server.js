1c4e6ab5cd3a0418e63674dea865e9496b656d77
Sandesh Shrestha <sandesh@tekkon.io>
2020-08-27 23:09:13 +0545

    file changes

--- server.js
+++ server.js
@@ -37,6 +37,18 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
+if (process.env.NODE_ENV === 'production') {
+    // Set a static folder
+    app.use(express.static(path.join(__dirname, "client/build")))
+
+    // Send all requests in build index
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+    });
+
+    console.log("Sent file to build index.html")
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server Started on port ${port}`));
